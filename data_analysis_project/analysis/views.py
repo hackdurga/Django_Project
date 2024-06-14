586edc48f2cfdbc93d35c6fbb4d816d3b85f0d73
Durga  Kumar <90327403+Durga69@users.noreply.github.com>
2024-06-14 17:24:46 +0530

    Create a view to handle file upload in analysis/views.py:

--- data_analysis_project/analysis/views.py
+++ data_analysis_project/analysis/views.py
@@ -1,3 +1,28 @@
+import os
+import pandas as pd
 from django.shortcuts import render
+from .forms import UploadFileForm
 
-# Create your views here.
+def handle_uploaded_file(file):
+    file_path = os.path.join('media', file.name)
+    with open(file_path, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+    return file_path
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_path = handle_uploaded_file(request.FILES['file'])
+            data = pd.read_csv(file_path)
+            # Perform data analysis here
+            summary_stats = data.describe().to_html()
+            first_rows = data.head().to_html()
+            return render(request, 'analysis/results.html', {
+                'summary_stats': summary_stats,
+                'first_rows': first_rows,
+            })
+    else:
+        form = UploadFileForm()
+    return render(request, 'analysis/upload.html', {'form': form})
