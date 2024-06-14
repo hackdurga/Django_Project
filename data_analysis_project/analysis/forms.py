1efd5bb409beeccd5248d759cb861cd829823082
Durga  Kumar <90327403+Durga69@users.noreply.github.com>
2024-06-14 19:15:39 +0530

    made some changes to make ui Styliss

--- data_analysis_project/analysis/forms.py
+++ data_analysis_project/analysis/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(label='Select a CSV file', widget=forms.FileInput(attrs={'class': 'form-control'}))
